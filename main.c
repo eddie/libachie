8abfca322e41dce60e1816da5a5fda80814db2a0
Cyclo <eddie@cyclo.local>
2012-11-15 19:39:06 +0000

    updated license and formatting

--- main.c
+++ main.c
@@ -1,3 +1,21 @@
+/*
+  libachie - backend library for point system for task orientated applications
+  Copyright (C) 2012 Eddie Blundell eblundell@gmail.com
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -11,7 +29,6 @@ struct ac_user{
   int points;
 
 };
-
 
 struct ac_group {
 
@@ -66,7 +83,7 @@ get_local_time()
 }
 
 struct ac_task*
-get_last_task(struct ac_instance *instance)
+ac_last_task(struct ac_instance *instance)
 {
   if(!instance){
     // die
@@ -89,7 +106,7 @@ get_last_task(struct ac_instance *instance)
 }
 
 struct ac_group*
-get_last_group(struct ac_instance *instance)
+ac_last_group(struct ac_instance *instance)
 {
   if(!instance){
     // Error
@@ -110,7 +127,7 @@ get_last_group(struct ac_instance *instance)
 }   
 
 void 
-add_task(struct ac_instance *instance,char *s_task)
+ac_add_task(struct ac_instance *instance,char *s_task)
 {
   if(!instance){
     // Error
@@ -133,7 +150,7 @@ add_task(struct ac_instance *instance,char *s_task)
 
   strcpy(task->task,s_task);
 
-  struct ac_task *prev = get_last_task(instance);
+  struct ac_task *prev = ac_last_task(instance);
   
   if(prev){
     task->prev = prev;
@@ -146,7 +163,7 @@ add_task(struct ac_instance *instance,char *s_task)
 }
 
 void
-add_group(struct ac_instance *instance, char *s_title,char *s_desc)
+ac_add_group(struct ac_instance *instance, char *s_title,char *s_desc)
 {
   if(!instance){
     // Error
@@ -161,7 +178,7 @@ add_group(struct ac_instance *instance, char *s_title,char *s_desc)
   strcpy(group->title,s_title);
   strcpy(group->description,s_desc);
 
-  struct ac_group *prev = get_last_group(instance);
+  struct ac_group *prev = ac_last_group(instance);
 
   if(prev){
     group->prev = prev;
@@ -174,9 +191,8 @@ add_group(struct ac_instance *instance, char *s_title,char *s_desc)
   }
   
 }
-
 struct ac_task*
-task_from_id(struct ac_instance *instance, u_int task_id)
+ac_task_from_id(struct ac_instance *instance, u_int task_id)
 {
   
   if(!instance){
@@ -199,7 +215,7 @@ task_from_id(struct ac_instance *instance, u_int task_id)
 }
 
 struct ac_group* 
-group_from_id(struct ac_instance *instance, u_int group_id)
+ac_group_from_id(struct ac_instance *instance, u_int group_id)
 {
   if(!instance){
     // Error
@@ -218,6 +234,47 @@ group_from_id(struct ac_instance *instance, u_int group_id)
   return NULL;
 }
 
+int 
+ac_count_tasks(struct ac_instance *instance)
+{
+  if(!instance){
+    // Error
+    return -1;
+  }
+
+  struct ac_task *tmp = instance->tasks;
+
+  if(!tmp){
+    return 0;
+  }
+
+  int i = 1;
+  while((tmp=tmp->next) && ++i)
+    ;;
+
+  return i;
+}
+
+int 
+ac_count_groups(struct ac_instance *instance)
+{
+  if(!instance){
+    // Error
+    return -1;
+  }
+
+  struct ac_group *tmp = instance->groups;
+
+  if(!tmp){
+    return 0;
+  }
+
+  int i = 1;
+  while((tmp=tmp->next) && ++i)
+    ;;
+
+  return i;
+}
 
 int
 date_diff(u_int timestamp){
@@ -231,7 +288,7 @@ date_diff(u_int timestamp){
 }
 
 void
-calculate_points(struct ac_instance *instance)
+ac_calculate_points(struct ac_instance *instance)
 {
 
   struct ac_task *tmp = instance->tasks;
@@ -270,14 +327,14 @@ next:
 }
 
 void 
-delete_task(struct ac_instance *instance, u_int task_id)
+ac_delete_task(struct ac_instance *instance, u_int task_id)
 {
   if(!instance){
     // Error
     return;
   }
 
-  struct ac_task *task = task_from_id(instance, task_id);
+  struct ac_task *task = ac_task_from_id(instance, task_id);
   
   if(!task){
     // Error
@@ -302,7 +359,7 @@ delete_task(struct ac_instance *instance, u_int task_id)
 
     
 void
-free_ac_instance(struct ac_instance *instance)
+ac_free_instance(struct ac_instance *instance)
 {
   if(instance->groups){
     struct ac_group *g_tmp = NULL;
@@ -338,7 +395,7 @@ free_ac_instance(struct ac_instance *instance)
 }
 
 void 
-save_ac_instance(struct ac_instance *instance,char *path)
+ac_save_instance(struct ac_instance *instance,char *path)
 {
   if(!instance){
     // Error
@@ -352,14 +409,103 @@ save_ac_instance(struct ac_instance *instance,char *path)
     // Error
     return;
   }
+
+  u_int tasks = ac_count_tasks(instance);
+  u_int groups = ac_count_groups(instance);
+
+  fwrite(instance->user,sizeof(struct ac_user),1,fp);
   
+  fwrite(&tasks, sizeof(u_int),1,fp);
+  fwrite(&groups, sizeof(u_int),1,fp);
+
+  fwrite(&instance->multiplier,sizeof(u_int),1,fp);
+  fwrite(&instance->threshold,sizeof(u_int),1,fp);
+
+  struct ac_group *g_tmp = instance->groups;
+
+  while(g_tmp){
+    fwrite(g_tmp,sizeof(struct ac_group),1,fp);
+    g_tmp = g_tmp->next;
+  }
+
+  struct ac_task *t_tmp = instance->tasks;
+
+  while(t_tmp){
+    fwrite(t_tmp,sizeof(struct ac_task),1,fp);
+    t_tmp = t_tmp->next;
+  }
 
 
   fclose(fp);
 }
 
+struct ac_instance*
+ac_load_instance(char *path)
+{
+  FILE *fp;
+  fp = fopen(path,"rb");
+
+  if(!fp){
+    // Error
+    return NULL;
+  }
+
+  struct ac_instance* instance;
+  instance = malloc(sizeof(struct ac_instance));
+  instance->tasks = NULL;
+  instance->groups = NULL;
+
+  // Read user info in
+  instance->user = malloc(sizeof(struct ac_user));
+  fread(instance->user,1,sizeof(struct ac_user),fp);
+
+  u_int groups,tasks;
+  groups = tasks = 0;
+
+  fread(&tasks,1,sizeof(u_int),fp);
+  fread(&groups,1,sizeof(u_int),fp);
+  
+  fread(&instance->threshold,1,sizeof(u_int),fp);
+  fread(&instance->multiplier,1,sizeof(u_int),fp);
+  
+  u_int i;
+  struct ac_group *g_tmp,*g_prev;
+  g_tmp = g_prev = NULL;
+  
+  for(i = 0; i < groups; i++){
+    
+    g_prev = g_tmp;
+
+    // TODO: Handle malloc error
+    g_tmp = malloc(sizeof(struct ac_group));
+    fread(g_tmp,sizeof(struct ac_group),1,fp);
+
+    if(g_prev){
+      g_prev->next = g_tmp;
+      g_tmp->prev = g_prev;
+    }
+  }
+  
+  struct ac_task *t_tmp, *t_prev;
+  t_tmp = t_prev = NULL;
+
+  for(i = 0; i < tasks; i++){
+    t_prev = t_tmp;
+
+    t_tmp = malloc(sizeof(struct ac_task));
+    fread(t_tmp,sizeof(struct ac_task),1,fp);
+
+    if(t_prev){
+      t_prev->next = t_tmp;
+      t_tmp->prev = t_prev;
+    }
+  }
+
+  fclose(fp);
+}
+
 struct ac_group*
-get_default_group()
+ac_default_group()
 {
   struct ac_group *tmp;
   tmp = malloc(sizeof(struct ac_group));
@@ -374,7 +520,7 @@ get_default_group()
 }
 
 void 
-dump_instance(struct ac_instance *instance)
+ac_dump_instance(struct ac_instance *instance)
 {
   if(!instance){
     // Error
@@ -384,14 +530,15 @@ dump_instance(struct ac_instance *instance)
   printf("User Points: %d\n",instance->user->points);
 
   struct ac_group *t_group;
+  struct ac_task *t_task;
 
-  // Dump out tasks
-  while(instance->tasks){
+  t_task = instance->tasks;
 
-    t_group = group_from_id(instance,instance->tasks->group);
-
-    printf("Task: %s Group: %s \n", instance->tasks->task,t_group->title);
-    instance->tasks = instance->tasks->next;
+  while(t_task){
+    
+    t_group = ac_group_from_id(instance,t_task->group);
+    printf("Task: %s Group: %s \n", t_task->task,t_group->title);
+    t_task = t_task->next;
   }
 
 }
@@ -422,26 +569,29 @@ int main()
   instance->threshold = 3;
   instance->user = user;
   instance->tasks = NULL;
-  instance->groups = get_default_group();
+  instance->groups = ac_default_group();
 
-  add_group(instance, "Work", "Work group");
-  add_task(instance, "Hello World!");
-  add_task(instance, "This is a test");
-  add_task(instance, "This is a another test");
-  add_task(instance, "This is a boom boom shake the room test");
+  ac_add_group(instance, "Work", "Work group");
+  ac_add_task(instance, "Hello World!");
+  ac_add_task(instance, "This is a test");
+  ac_add_task(instance, "This is a another test");
+  ac_add_task(instance, "This is a boom boom shake the room test");
 
   instance->tasks->complete = 1;
   instance->tasks->group = 1;
   instance->tasks->creation_date = get_local_time();
   instance->tasks->next->complete = 1;
 
-  delete_task(instance,2);
+  ac_delete_task(instance,2);
 
-  calculate_points(instance);
+  ac_calculate_points(instance);
+  
+  ac_save_instance(instance,"test.ac");
+  ac_load_instance("test.ac");
 
-  dump_instance(instance);
+  ac_dump_instance(instance);
 
-  free_ac_instance(instance);
+  ac_free_instance(instance);
 
   free(user);
   free(instance);
